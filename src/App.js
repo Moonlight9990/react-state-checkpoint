d7f338ef8e47ce8ae6ee2d37e3cd3ee047db4d79
Moonlight9990 <lioswag6@gmail.com>
2023-08-22 11:27:50 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+// class app extends React.Component 
+class App extends Component {
+  state = {
+    person: {
+      fullName: 'John Doe',
+      bio: 'A passionate developer',
+      imgSrc: 'https://example.com/profile.jpg',
+      profession: 'Software Engineer'
+    },
+    show: false,
+    mountTime: new Date()
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // for toggling the profile
+  toggleProfile = () => {
+    this.setState((prevState) => ({
+      show: !prevState.show
+    }));
+  };
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({ mountTime: new Date() });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    const { person, show, mountTime } = this.state;
+
+    // the display
+
+    return (
+      <div className="App">
+        <button onClick={this.toggleProfile}>
+          {show ? 'Hide Profile' : 'Show Profile'}
+        </button>
+        {show && (
+          <div>
+            <h2>{person.fullName}</h2>
+            <p>{person.bio}</p>
+            <img src={person.imgSrc} alt="Profile" />
+            <p>Profession: {person.profession}</p>
+          </div>
+        )}
+        <p>Time since mount: {Math.floor((new Date() - mountTime) / 1000)} seconds</p>
+      </div>
+    );
+  }
 }
 
 export default App;
